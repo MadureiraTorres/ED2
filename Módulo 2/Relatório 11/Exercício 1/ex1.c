e5c83f8c5093c5efa3a6c9a9a026c42fa92be367
Rafael Torres <rafaeltorres.2020@alunos.utfpr.edu.br>
2026-05-14 13:57:14 -0300

    Refactor ex1.c to use a constant for the number of people and improve file writing error handlinggit

--- Módulo 2/Relatório 11/Exercício 1/ex1.c
+++ Módulo 2/Relatório 11/Exercício 1/ex1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_PESSOAS 4
+
 typedef struct p{
     char nome[30];
     int idade;
@@ -9,21 +11,26 @@ typedef struct p{
 
 int main(){
     FILE *arq;
-    Pessoa pessoas[4] = {
+    Pessoa pessoas[NUM_PESSOAS] = {
         {"Rafael", 24, 90},
         {"Joice", 24, 60},
         {"Cristiano", 40, 81.8},
         {"Fallen", 36, 92.7}
     };
 
-    arq = fopen("cadastro.txt", "wb");
+    arq = fopen("cadastro.txt", "w");
 
     if(arq == NULL){
         printf("Erro ao abrir arquivo!\n");
         exit(1);
     }
 
-    arq = fwrite(pessoas, sizeof(Pessoa), 4, arq);
+    size_t elementosEscritos = fwrite(pessoas, sizeof(Pessoa), NUM_PESSOAS, arq);
+    if (elementosEscritos != NUM_PESSOAS) {
+        printf("Erro ao escrever no arquivo! Apenas %zu de %d elementos foram escritos.\n", elementosEscritos, NUM_PESSOAS);
+        fclose(arq);
+        exit(1);
+    }
 
     fclose(arq);
     
